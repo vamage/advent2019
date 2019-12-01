e15b7848cdeab09327c74f5ffc094bc00a13baf9
vamage <vamage+spam@vamage.com>
2019-12-01 18:20:11 -0500

    fixing fmt

--- day1/main.go
+++ day1/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	var fuel  = 0
+	var fuel = 0
 	var fuel2 = 0
 	file, err := os.Open("part1.input")
 	if err != nil {
@@ -17,35 +17,34 @@ func main() {
 	}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		mass,err := strconv.Atoi(scanner.Text())
+		mass, err := strconv.Atoi(scanner.Text())
 
-		if  err != nil {
+		if err != nil {
 			fmt.Fprintln(os.Stderr, "not a int ", err)
 		}
-		 fuel += masscalc(mass)
-		 fuel2 += masscalc2(mass)
+		fuel += masscalc(mass)
+		fuel2 += masscalc2(mass)
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 
-	fmt.Fprintf(os.Stdout,"fuel needed is %d\n",fuel)
-	fmt.Fprintf(os.Stdout,"fuel2 needed is %d\n",fuel2)
+	fmt.Fprintf(os.Stdout, "fuel needed is %d\n", fuel)
+	fmt.Fprintf(os.Stdout, "fuel2 needed is %d\n", fuel2)
 }
 
-func masscalc(mass int)(int)  {
+func masscalc(mass int) int {
 
-
-	return (mass/3)-2
+	return (mass / 3) - 2
 
 }
-func masscalc2(mass int) (int) {
+func masscalc2(mass int) int {
 
 	fuel := (mass / 3) - 2
 	if fuel > 0 {
 		return fuel + masscalc2(fuel)
 	} else {
 		return 0
-}
+	}
 
 }
